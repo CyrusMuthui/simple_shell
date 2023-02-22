a7cb15f796cb41571b50686c332e918df2f1581d
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-02-22 22:59:03 +0300

    Simple Shell

--- errors.c
+++ errors.c
@@ -45,7 +45,7 @@ int _eputchar(char c)
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
- *
+ * 345
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
